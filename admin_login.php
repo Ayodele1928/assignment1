ea0f8496876dc1d58a8c0e173b28e30280614526
Ayodele <ayodele.owolabi@gmail.com>
2017-03-18 23:51:20 +0100

    Finish assignment

---
+++ admin_login.php
@@ -0,0 +1,80 @@
+<?php
+ $db = mysqli_connect("localhost", "root", "@@yodele", "o_banking") or die(mysqli_error());
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Admin Login</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+ 	<?php
+ 		if(array_key_exists('login', $_POST)){ //for login authentication
+ 			$error = array();
+
+ 			if(empty($_POST['admin_name'])){ //username
+ 				$error[] = "Please enter your Username";
+ 			} else {
+ 				$uname = (mysqli_real_escape_string($db, $_POST['admin_name']));
+ 			}
+
+ 			if(empty($_POST['password'])){ //password
+ 				$error[] = "Please enter your Password";
+ 			} else {
+ 				$pword = md5(mysqli_real_escape_string($db, $_POST['password']));
+ 			}
+
+ 			if (empty($error)) { //IF NO ERRORS on login
+ 				$query = mysqli_query($db, "SELECT * FROM admin WHERE
+											admin_name = '".$uname."' AND
+ 											password = '".$pword."'")
+											or die(mysqli_error($db));
+				
+				
+				
+				if(mysqli_num_rows($query) == 1){ //if record returned = 1
+					while ($admin_detail = mysqli_fetch_array($query)) {
+						//establish session for user
+						$_SESSION['id'] = $admin_detail['admin_id'];
+						$_SESSION['admin_name'] = $admin_detail['admin_name'];
+						header("Location:admin_home.php"); //Log user in (Access Granted)
+					}
+				} else { //If record returned is not = 1
+					$login_error = "Invalid Username and/or Password";
+					header("Location: admin_login.php?login_error=$login_error");
+				}
+				
+				} 	else{
+ 					foreach ($error as $error) {
+
+ 					echo '<p>'.$error.'</p>';
+ 				}
+ 			}
+ 		}
+
+
+ 		if(isset($_GET['login_error'])){
+ 		echo $_GET['login_error'];
+ 		}
+
+ 	?>
+ 	<h3> Welcome Admin </h3>
+ 	<form action="" method="post">
+ 		<legend>
+ 			<fieldset>
+ 			<legend> Login </legend>
+ 				<p> Username: <input type="text" name="admin_name"/></p>
+ 				<p> Password: <input type="password" name="password"/></p>
+ 				<input type="submit" name="login" value="Login" /></p>
+ 			
+ 			</fieldset>
+ 		</legend>
+ 	</form>
+ 	
+
+
+
+</body>
+</html>
---
+++ admin_register.php
@@ -0,0 +1,68 @@
+<?php
+ $db = mysqli_connect("localhost", "root", "@@yodele", "o_banking") or die(mysqli_error());
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title> Admin </title>
+</head>
+<body>
+<h3> Add Users </h3>
+<?php
+	if (array_key_exists('login', $_POST)){
+		$error = array();
+
+		if (empty($_POST['admin_name'])){
+		$error[] = "Enter a new admin name";
+		} else{
+			$admin_name = mysqli_real_escape_string($db, $_POST['admin_name']);
+		}
+
+		if (empty($_POST['password'])) {
+			$error[] = "Enter admin password";
+		} else{
+			$pword = md5(mysqli_real_escape_string($db, $_POST['password']));
+		}
+		
+		if(empty($error)){
+			$insert = mysqli_query($db, "INSERT INTO admin
+
+									VALUES(NULL, 
+											'".$admin_name."',
+											'".$pword."'
+											)") or die(mysqli_error($db));
+											
+											
+
+			$success = "Admin successfully created";
+		header("Location:admin_register.php?success=$success");
+		}
+		else{
+			foreach ($error as $error) {
+				echo '<p><strong>'.$error.'</strong></p>';
+			}
+
+		}
+	}
+
+if(isset($_GET['success'])){
+	echo '<p><em>'.$_GET['success'].'</em></p>';
+}
+?>
+ 	<form action="" method="post">
+ 		<legend>
+ 			<fieldset>
+ 			<legend> Add Admin </legend>
+ 				<p> Admin Name: <input type="text" name="admin_name"/></p>
+ 				<p> Password: <input type="password" name="password"/></p>
+ 				<input type="submit" name="login" value="Add Admin" /></p>
+ 			
+ 			</fieldset>
+ 		</legend>
+ 	</form>
+ 	
+
+</body>
+</html>
